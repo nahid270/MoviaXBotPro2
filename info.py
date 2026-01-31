4bf31fdf504e0c7612ce28493404551b50707a2a
nahid270 <110238003+nahid270@users.noreply.github.com>
2026-01-31 20:44:17 +0600

    Update info.py

--- info.py
+++ info.py
@@ -59,7 +59,7 @@ UPDATE_CHANNEL_LNK = environ.get('UPDATE_CHANNEL_LNK', 'https://t.me/TGLinkBase'
 AUTH_CHANNEL = environ.get("AUTH_CHANNEL", "-1003356899562") # add multiple channels here, separated by single space
 AUTH_REQ_CHANNEL = environ.get('AUTH_REQ_CHANNEL', '-1003586737087')
 
-IS_VERIFY = is_enabled('IS_VERIFY', False)
+IS_VERIFY = is_enabled('IS_VERIFY', True)
 LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '-1003889628520'))
 LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '-1003463909746'))
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://telegra.ph/file/9ecc5d6e4df5b83424896.jpg")
